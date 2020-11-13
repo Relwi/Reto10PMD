9bfcfb7449351be591b4f80dd6590f3fd8d8b4de
Iker <theofficialdork@hotmail.com>
2020-11-13 11:35:52 +0100

    A

--- app/src/main/java/com/example/practica8intentsexplicitos/DashboardActivity.java
+++ app/src/main/java/com/example/practica8intentsexplicitos/DashboardActivity.java
@@ -2,12 +2,14 @@ package com.example.practica8intentsexplicitos;
 
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.Locale;
 
@@ -15,23 +17,37 @@ public class DashboardActivity extends AppCompatActivity {
     private ArrayList<String> alWordsEnglish = new ArrayList<>();
     private ArrayList<String> alWordsSpanish = new ArrayList<>();
     private TextView textView = null;
+    private EditText editText = null;
+    private Button button = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
+        // Limpiamos los ArrayList por si en la base de datos implementamos más palabras
         alWordsEnglish.clear();
         alWordsSpanish.clear();
-        textView = (TextView) findViewById(R.id.dashboard_TextView);
 
-        if (Locale.getDefault().getLanguage().equals("en")) {
-            cursorWordsEnglish();
-            int a = (int) (Math.random() * (4 - 0 + 1) + 0);
-            textView.setText(alWordsEnglish.get(a));
-        } else if (Locale.getDefault().getLanguage().equals("es")) {
+        textView = (TextView) findViewById(R.id.dashboard_TextView);
+        editText = (EditText) findViewById(R.id.dashboard_EditText);
+        button = (Button) findViewById(R.id.dashboard_Button);
+        editText.requestFocus();
+
+        if (Locale.getDefault().getLanguage().equals("es")) {
             cursorWordsSpanish();
-        } // TODO: Si todo falla, volver al MainActiviy sacando un mensaje de error
+            a();
+        } else {
+            cursorWordsEnglish();
+            a();
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (textView.getText().toString().equals(editText.getText().toString()))
+                        a();
+                }
+            });
+        }
     }
 
     private void cursorWordsEnglish() {
@@ -48,5 +64,12 @@ public class DashboardActivity extends AppCompatActivity {
             alWordsSpanish.add(cursorWordsSpanish.getString(cursorWordsSpanish.getColumnIndex("wordEnglish")));
         }
         cursorWordsSpanish.close();
+    }
+
+    private void a() {
+        int a = (int) (Math.random() * (4 - 0 + 1) + 0);
+        editText.getText().clear();
+        textView.clearComposingText();
+        textView.setText(alWordsEnglish.get(a));
     }
 }
